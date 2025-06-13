17fe6de54f6cc81ec19d743aba0c0634dd3a08c5
kbyunghoon <dev.kbhoon@gmail.com>
2025-06-13 19:09:37 +0900

    feat: 사용자 대기열 컨트롤러 추가

---
+++ src/main/java/com/concertqueuesystem/flow/controller/UserQueueController.java
@@ -0,0 +1,44 @@
+package com.concertqueuesystem.flow.controller;
+
+import com.concertqueuesystem.flow.dto.AllowUserResponse;
+import com.concertqueuesystem.flow.dto.AllowedUserResponse;
+import com.concertqueuesystem.flow.dto.RegisterUserResponse;
+import com.concertqueuesystem.flow.service.UserQueueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/queue")
+@RequiredArgsConstructor
+public class UserQueueController {
+    private final UserQueueService userQueueService;
+
+    @PostMapping("")
+    public Mono<RegisterUserResponse> registerUser(
+            @RequestParam(name = "queue", defaultValue = "default") String queue,
+            @RequestParam(name = "user_id") Long userId) {
+        return userQueueService.registerWaitQueue(queue, userId)
+                .map(RegisterUserResponse::new);
+    }
+
+    @PostMapping("/allow")
+    public Mono<AllowUserResponse> allowUsers(
+            @RequestParam(name = "queue", defaultValue = "default") String queue,
+            @RequestParam(name = "count") Long count) {
+        return userQueueService.allowUser(queue, count)
+                .map(allowed -> new AllowUserResponse(count, allowed));
+    }
+
+    @GetMapping("/allowed")
+    public Mono<AllowedUserResponse> isAllwoed(
+            @RequestParam(name = "queue", defaultValue = "default") String queue,
+            @RequestParam(name = "user_id") Long userId) {
+        return userQueueService.isAllowed(queue, userId)
+                .map(AllowedUserResponse::new);
+    }
+}
