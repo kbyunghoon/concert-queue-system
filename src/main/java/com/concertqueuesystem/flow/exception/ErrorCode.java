a8adcbd5715471a54b6dc0ca6ffb409e54f63c9e
kbyunghoon <dev.kbhoon@gmail.com>
2025-06-13 19:08:23 +0900

    feat: 에러 코드 정의 추가

---
+++ src/main/java/com/concertqueuesystem/flow/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.concertqueuesystem.flow.exception;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+public enum ErrorCode {
+    QUEUE_ALREADY_REGISTERED_USER(HttpStatus.CONFLICT, "UQ-001", "Already registered in queue");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String reason;
+
+    public ApplicationException build() {
+        return new ApplicationException(httpStatus, code, reason);
+    }
+
+    public ApplicationException build(Object ...args) {
+        return new ApplicationException(httpStatus, code, reason);
+    }
+}
