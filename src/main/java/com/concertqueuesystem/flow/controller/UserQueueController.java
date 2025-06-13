4028669941080a5648306107cc5393e5b57c9d71
kbyunghoon <dev.kbhoon@gmail.com>
2025-06-14 02:23:57 +0900

    feat: 대기열 순번 컨트롤러 추가

--- src/main/java/com/concertqueuesystem/flow/controller/UserQueueController.java
+++ src/main/java/com/concertqueuesystem/flow/controller/UserQueueController.java
@@ -2,6 +2,7 @@ package com.concertqueuesystem.flow.controller;
 
 import com.concertqueuesystem.flow.dto.AllowUserResponse;
 import com.concertqueuesystem.flow.dto.AllowedUserResponse;
+import com.concertqueuesystem.flow.dto.RankNumberResponse;
 import com.concertqueuesystem.flow.dto.RegisterUserResponse;
 import com.concertqueuesystem.flow.service.UserQueueService;
 import lombok.RequiredArgsConstructor;
@@ -40,5 +41,13 @@ public class UserQueueController {
             @RequestParam(name = "user_id") Long userId) {
         return userQueueService.isAllowed(queue, userId)
                 .map(AllowedUserResponse::new);
+    }
+
+    @GetMapping("/rank")
+    public Mono<RankNumberResponse> getRankUser(
+            @RequestParam(name = "queue", defaultValue = "default") String queue,
+            @RequestParam(name = "user_id") Long userId) {
+        return userQueueService.getRank(queue, userId)
+                .map(RankNumberResponse::new);
     }
 }
